e3e91360e55a4f2a7b38c3c2106d17768a532139
fluixyz-unnatural <sudo3949@gmail.com>
2021-11-18 00:16:08 +0900

    トップページをスタイリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Box, VStack, chakra, Link } from "@chakra-ui/react"
+import { Box, Divider, VStack, HStack, chakra, Link } from "@chakra-ui/react"
 
 export default function Home() {
   return (
-    <div>
-      <chakra.h1 pointerEvents="none" fontSize={32} textAlign='center'>Fluixyz</chakra.h1>
-      <VStack>
-        <Box><Link href="/blog">blog</Link></Box>
-        <Box><Link href="/works">works</Link></Box>
+    <Box m={20}>
+      <VStack spacing="30px">
+        <chakra.h1 pointerEvents="none" fontSize={"4rem"} textAlign='center' >Fluixyz</chakra.h1>
+        <Divider />
+        <VStack>
+          <Box ><Link fontSize="1.5rem" href="/blog">blog</Link></Box>
+          <Box><Link fontSize="1.5rem" href="/works">works</Link></Box>
+        </VStack>
+        <Divider />
+        <HStack>
+          <Link href="https://github.com/fluixyz-unnatural">github</Link>
+          <Link href="https://twitter.com/higara333">twitter</Link>
+        </HStack>
       </VStack>
-    </div>
+    </Box>
   );
 }
