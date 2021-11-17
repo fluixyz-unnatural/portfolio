fd586260cb8b406cda791335288782a5b56a5bce
fluixyz-unnatural <sudo3949@gmail.com>
2021-11-17 17:29:15 +0900

    記事カード作成

--- src/components/articleCard.tsx
+++ src/components/articleCard.tsx
@@ -1,8 +1,25 @@
-import { Box } from "@chakra-ui/react"
+import { Box, VStack, HStack, Image, Divider, Heading, Text, Link } from "@chakra-ui/react"
 
-function ArticleCard(){
+interface Props {
+    title: string;
+    summary: string;
+    thumbnail: string;
+    to: string;
+}
+
+function ArticleCard(props: Props) {
     return (
-        <Box></Box>
+        <Box shadow="md" borderRadius={10} overflow="hidden" width="full">
+            <HStack as={Link} href={props.to} >
+                <Image boxSize="100px" objectFit="cover" src={props.thumbnail} />
+                <VStack align="left" textAlign="left" w="full">
+                    <Heading size="md">{props.title}</Heading>
+                    <Divider />
+                    <Text size="sm" fontSize="0.8rem">{props.summary}</Text>
+                </VStack>
+            </HStack>
+        </Box>
     )
 }
+
 export default ArticleCard
