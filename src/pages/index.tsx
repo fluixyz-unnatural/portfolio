c99e9ddf8f1fa24268ed91839a166243da7ecbd7
fluixyz-unnatural <sudo3949@gmail.com>
2021-11-18 16:33:22 +0900

    トップページのタイトル、OGPを設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import Head from 'next/head'
 import { Box, Divider, VStack, HStack, chakra, Link } from "@chakra-ui/react"
 
 export default function Home() {
   return (
-    <Box m={20}>
-      <VStack spacing="30px">
-        <chakra.h1 fontFamily={"Fjalla One"} pointerEvents="none" fontSize={"4rem"} textAlign='center' >fluixyz</chakra.h1>
-        <Divider />
-        <VStack>
-          <Box ><Link fontSize="1.5rem" href="/blog">blog</Link></Box>
-          <Box><Link fontSize="1.5rem" href="/works">works</Link></Box>
+    <>
+      <Head>
+        <title>fluixyz</title>
+        <meta name="description" content="fluixyzのブログ兼ポートフォリオサイトです。"></meta>
+        <meta name="og:description" content="fluixyzのブログ兼ポートフォリオサイトです。"></meta>
+        <meta name="og:title" content="fluixyz"></meta>
+      </Head>
+      <Box m={20}>
+        <VStack spacing="30px">
+          <chakra.h1 fontFamily={"Fjalla One"} pointerEvents="none" fontSize={"4rem"} textAlign='center' >fluixyz</chakra.h1>
+          <Divider />
+          <VStack>
+            <Box ><Link fontSize="1.5rem" href="/blog">blog</Link></Box>
+            <Box><Link fontSize="1.5rem" href="/works">works</Link></Box>
+          </VStack>
+          <Divider />
+          <HStack>
+            <Link href="https://github.com/fluixyz-unnatural">github</Link>
+            <Link href="https://twitter.com/higara333">twitter</Link>
+          </HStack>
         </VStack>
-        <Divider />
-        <HStack>
-          <Link href="https://github.com/fluixyz-unnatural">github</Link>
-          <Link href="https://twitter.com/higara333">twitter</Link>
-        </HStack>
-      </VStack>
-    </Box>
+      </Box>
+    </>
   );
 }
