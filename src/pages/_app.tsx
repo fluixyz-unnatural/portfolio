5367d61b16d03a3f70bc68e7919507f857f04303
fluixyz-unnatural <sudo3949@gmail.com>
2021-11-18 10:58:40 +0900

    スタイリング

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react"
+import '../styles/article.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
