84a0c3ea6014fb3943672e3570966bff15763acc
fluixyz-unnatural <sudo3949@gmail.com>
2021-12-05 01:26:17 +0900

    ヘッダーリンク先変更

--- src/components/blogHeader.tsx
+++ src/components/blogHeader.tsx
@@ -2,7 +2,7 @@ import { Heading, Divider, Link } from "@chakra-ui/react";
 
 const BlogHeader = () => {
     return (
-        <Link href="/blog" >
+        <Link href="/blog/page/1" >
             <Heading fontFamily={"Fjalla One"} p="2rem" textAlign="center">{"fluixyz's log"}</Heading>
             <Divider mb="2rem" />
         </Link>
