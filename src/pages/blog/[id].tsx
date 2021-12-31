748e4a2eefbed5010f632edb92eb881814e00a78
fluixyz-unnatural <sudo3949@gmail.com>
2021-12-31 14:08:40 +0900

    a

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 import { Box, Heading, Text, VStack, HStack } from "@chakra-ui/react"
 import { client } from "../../libs/client"
@@ -10,7 +9,7 @@ import Head from "next/head"
 export const getStaticPaths = async () => {
     const data: MicroCMSIdsList = await client.get({
         endpoint: 'article',
-        queries: { fields: 'id' }
+        queries: { fields: 'id', limit: 1000 }
     })
     const paths = data.contents.map((elm: IdObject) => '/blog/' + elm.id)
     return { paths, fallback: false }
@@ -74,82 +73,4 @@ function Article({ article }: Props) {
     )
 }
 
-=======
-import React from "react"
-import { Box, Heading, Text, VStack, HStack } from "@chakra-ui/react"
-import { client } from "../../libs/client"
-import BlogHeader from "src/components/BlogHeader"
-import { MicroCMSListContent } from "microcms-js-sdk"
-import { BlogType, MicroCMSIdsList, IdObject } from "src/types/microcms"
-import Head from "next/head"
-
-export const getStaticPaths = async () => {
-    const data: MicroCMSIdsList = await client.get({
-        endpoint: 'article',
-        queries: { fields: 'id', limit: 1000 }
-    })
-    const paths = data.contents.map((elm: IdObject) => '/blog/' + elm.id)
-    return { paths, fallback: false }
-}
-
-// TODO わからんぴ
-interface nani {
-    id: string;
-}
-interface nanikore {
-    params: nani;
-}
-
-export const getStaticProps = async ({ params }: nanikore) => {
-    const data = await client.get({
-        endpoint: 'article',
-        queries: { ids: params.id }
-    })
-
-    return ({
-        props: {
-            article: data.contents[0],
-        }
-    })
-}
-
-interface Props {
-    article: (BlogType & MicroCMSListContent)
-}
-
-function Article({ article }: Props) {
-    return (
-        <>
-            <Head>
-                <title>{`${article.title} | fluixyz'log`}</title>
-                <meta name="og:type" content="article" />
-                <meta name="twitter:card" content="summary" />
-                <meta name="og:image" content={`${article.thumbnail}`} />
-                <meta name="og:title" content={`${article.title} | fluixyz'log`} />
-            </Head>
-            <BlogHeader />
-            <Box mt={10} maxW="630px" m="auto" >
-                <VStack align="flex-start" spacing="2rem" m="20px">
-                    <Box>
-                        <Heading mb="1rem" fontSize="1.6rem" textAlign="left">
-                            {article.title}
-                        </Heading>
-                        <HStack>
-                            <Text fontSize="sm">published: {article.publishedAt.split('T')[0]}</Text>
-                            <Text fontSize="sm">revised: {article.revisedAt.split('T')[0]}</Text>
-                            <Text fontSize="sm">カテゴリ: {article.category.map((elm: any) => elm.name)}</Text>
-                        </HStack>
-                    </Box>
-                    <Box>
-                        <div className="blog-article" dangerouslySetInnerHTML={{
-                            __html: `${article.article}`,
-                        }} />
-                    </Box>
-                </VStack>
-            </Box>
-        </>
-    )
-}
-
->>>>>>> 6d6a80ae9b176d14b308b19716834a035ac8c829
 export default Article
