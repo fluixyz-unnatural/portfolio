aabb66817fe51911a7d4ad391229411fda861622
fluixyz-unnatural <sudo3949@gmail.com>
2022-03-06 20:47:04 +0900

    フォント指定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Yanone+Kaffeesatz:wght@200&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Yanone+Kaffeesatz:wght@200&display=swap&text=fluixyz'slogwork"
             rel="stylesheet"
           />
         </Head>
