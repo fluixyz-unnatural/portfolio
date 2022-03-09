f8353186423d2dd99adb69f2974f20674b9f8018
fluixyz-unnatural <sudo3949@gmail.com>
2022-03-09 18:05:24 +0900

    lang=ja

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='ja'>
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Yanone+Kaffeesatz:wght@200&display=swap&text=fluixyz'slogwork"
