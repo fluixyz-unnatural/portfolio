8ce716416a362d059023b3b44f0c29e0d5c2cb2b
fluixyz-unnatural <sudo3949@gmail.com>
2023-01-01 16:08:24 +0900

    next fontを使ってみる

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import { AppProps } from 'next/app'
 import Link from 'next/link'
-import Script from 'next/script'
 import '../styles/index.css'
 import 'src/styles/article.css'
 import 'src/styles/top.css'
 import 'src/styles/blog.css'
 import 'src/styles/works.css'
 import { useRouter } from 'next/router'
+import { Fjalla_One } from '@next/font/google'
 import { useEffect, useRef } from 'react'
 import GoogleTagManager from 'src/components/GoogleTagManager'
+
+const Fjalla_One_normal = Fjalla_One({
+  weight: '400',
+  display: 'swap',
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -27,7 +32,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div ref={focusDummy} className="focus-dummy" tabIndex={-1}>
         focus
       </div>
-      <main>
+      <main
+        style={{ '--fjalla_one': Fjalla_One_normal.style.fontFamily } as any}
+      >
         <Component {...pageProps} />
         <footer className="footer">
           <Link href="/">fluixyz</Link>
