81d623a879b1662c5847b8c251fec3e9db6d3574
fluixyz-unnatural <sudo3949@gmail.com>
2023-02-14 15:59:13 +0900

    🗑

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link'
 
-const SubHeader = (props: { label: string; href: string }) => {
+const SubHeader = (props: { label: string; href?: string }) => {
   return (
     <div className={'sub-header'}>
-      <Link passHref href={props.href}>
+      {props.href ? (
+        <Link passHref href={props.href}>
+          <h1>{props.label}</h1>
+        </Link>
+      ) : (
         <h1>{props.label}</h1>
-      </Link>
+      )}
     </div>
   )
 }
